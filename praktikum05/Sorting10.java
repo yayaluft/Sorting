733b89a7a66a0d424252e45a96bf62f3ffc17f49
yayaluft <syanuha66@gmail.com>
2026-04-01 09:53:28 +0700

    Praktikum 1 Sorting - Insertion Sort

--- praktikum05/Sorting10.java
+++ praktikum05/Sorting10.java
@@ -4,17 +4,17 @@ public class Sorting10 {
     int[] data;
     int jumData;
 
-    Sorting10 (int Data[], int jmlDat){
-        jumData= jmlDat;
+    Sorting10(int Data[], int jmlDat) {
+        jumData = jmlDat;
         data = new int[jmlDat];
-        for (int i=0; i < jumData; i++){
+        for (int i = 0; i < jumData; i++) {
             data[i] = Data[i];
         }
     }
 
     void bubbleSort() {
         int temp = 0;
-        for (int i = 0; i < jumData-1; i++) {
+        for (int i = 0; i < jumData - 1; i++) {
             for (int j = 1; j < jumData - i; j++) {
                 if (data[j - 1] > data[j]) {
                     temp = data[j];
@@ -24,20 +24,34 @@ public class Sorting10 {
             }
         }
     }
-    void tampil(){
-        for(int i=0; i< jumData; i++){
-            System.out.print(data[i]+" ");
+
+    void tampil() {
+        for (int i = 0; i < jumData; i++) {
+            System.out.print(data[i] + " ");
         }
         System.out.println();
     }
-    void SelectionSort(){
-        for (int i=0; i < jumData-1; i++){
-            int min=i;
-            for (int j=i+1; j< jumData; j++){
-                if(data[j]<data[min]){
-                    min=j;
+
+    void SelectionSort() {
+        for (int i = 0; i < jumData - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < jumData; j++) {
+                if (data[j] < data[min]) {
+                    min = j;
                 }
             }
+        }
+    }
+
+    void insertionSOrt() {
+        for (int i = 1; i <= data.length - 1; i++) {
+            int temp = data[i];
+            int j = i - 1;
+            while (j >= 0 && data[j] > temp) {
+                data[j + 1] = data[j];
+                j--;
+            }
+            data[j+1]=temp;
         }
     }
 }
--- praktikum05/SortingMain10.java
+++ praktikum05/SortingMain10.java
@@ -19,5 +19,13 @@ public class SortingMain10 {
         dataurut2.SelectionSort();
         System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataurut2.tampil();
+
+        int c[]= {40, 10, 4, 9, 3};
+        Sorting10 dataurut3 = new Sorting10(c, c.length);
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSOrt();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
     }
 }
